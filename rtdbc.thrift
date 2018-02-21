2c62944d66b08706adfb97a6a1b5638918af16a8
vwarship <vwarship@163.com>
2018-02-22 03:47:15 +0800

    完善了接口

--- rtdbc.thrift
+++ rtdbc.thrift
@@ -4,12 +4,9 @@ namespace py rtdbc
 
 enum DataType
 {
-	DIGITAL = 1,
-	INT16,
-	INT32,
-	FLOAT32,
-	FLOAT64,
-	TIMESTAMP,
+	DIGITAL = 0,
+	INTEGER,
+	DOUBLE,
 	STRING,
 	BLOB,
 }
@@ -19,13 +16,15 @@ enum DataQuality
 	GOOD = 0,       //好
 	BAD,            //坏
 	UNCERTAIN,      //可疑
-	UNDEFINED,      //未定义
+	NA,             //未定义
 }
 
-enum SamplingMode
+union Value
 {
-    INTERPOLATED = 1,
-    TREND,
+	1: i64 Integer,
+	2: double Double,
+	3: string String,
+	4: binary Blob,
 }
 
 struct Error
@@ -35,28 +34,39 @@ struct Error
 	3: optional string message,
 }
 
-struct Data
+struct DataSample
 {
 	1: required string tagname,
 	2: required double timestamp,
 	3: required DataType type,
-	4: required double value,
+	4: required Value value,
 	5: required DataQuality dataQuality,
-	6: optional Error error,
+}
+
+struct DataRecord
+{
+	1: required DataSample dataSample,
+	2: required Error error,
+}
+
+enum SamplingMode
+{
+    INTERPOLATED = 0,
+    TREND,
 }
 
 exception IOError {
-  1: optional string message,
+  1: optional Error error,
 }
 
 exception IllegalArgument {
-  1: optional string message,
+  1: optional Error error,
 }
 
 service RTDBCService {
-    list<Data> readCurrentData(1:list<string> tagnames) throws (1:IOError io, 2:IllegalArgument ia),
-    list<Data> readInterpolatedData(1:list<string> tagnames, 2:list<double> timestamps) throws (1:IOError io, 2:IllegalArgument ia),
-    list<Data> readRawData(1:string tagname, 2:double startTime, 3:double endTime) throws (1:IOError io, 2:IllegalArgument ia),
-    list<Data> readSampledData(1:string tagname, 2:double startTime, 3:double endTime, 4:SamplingMode mode, 5:i64 intervalMilliseconds) throws (1:IOError io, 2:IllegalArgument ia),
-    list<Error> writeData(1:list<Data> datas) throws (1:IOError io, 2:IllegalArgument ia),
+    list<DataRecord> readCurrentDatas(1:list<string> tagnames) throws (1:IOError io, 2:IllegalArgument ia),
+    list<DataRecord> readInterpolatedDatas(1:list<string> tagnames, 2:list<double> timestamps) throws (1:IOError io, 2:IllegalArgument ia),
+    list<DataSample> readRawDatas(1:string tagname, 2:double startTime, 3:double endTime) throws (1:IOError io, 2:IllegalArgument ia),
+    list<DataSample> readSampledDatas(1:string tagname, 2:double startTime, 3:double endTime, 4:SamplingMode mode, 5:i64 intervalMilliseconds) throws (1:IOError io, 2:IllegalArgument ia),
+    list<Error> writeDatas(1:list<DataSample> dataSamples) throws (1:IOError io, 2:IllegalArgument ia),
 }
