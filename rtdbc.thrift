c9610e70576cfe6250693eae3fdf310d55ec924a
vwarship <vwarship@163.com>
2018-02-24 18:31:06 +0800

    增加检索类型和测点没有找到异常

--- rtdbc.thrift
+++ rtdbc.thrift
@@ -13,11 +13,20 @@ enum DataQuality {
 	BAD,            //坏
 }
 
-enum SamplingMode {
+/** 检索类型 */
+enum RetrievalType {
+    INTERPOLATED = 1,
+    BEFORE,
+    AFTER,
+}
+
+/** 采样类型 */
+enum SamplingType {
     INTERPOLATED = 1,
     TREND,
 }
 
+/** 数据采样 */
 struct DataSample {
 	1: required string tagname,
 	2: required double timestamp,
@@ -31,26 +40,32 @@ exception InvalidRequestException {
     1: optional string why,
 }
 
+/** 测点不存在 */
+exception TagNotFoundException {
+    1: optional string why,
+}
+
 service RTDBCService {
     list<DataSample> read_current_datas(1:required list<string> tagnames)
-        throws (1:InvalidRequestException ire),
+        throws (1:InvalidRequestException ire, 2:TagNotFoundException tnfe),
         
     list<DataSample> read_interpolated_datas(1:required list<string> tagnames, 
-                                             2:required double timestamp)
-        throws (1:InvalidRequestException ire),
+                                             2:required double timestamp,
+                                             3:required RetrievalType type=RetrievalType.INTERPOLATED)
+        throws (1:InvalidRequestException ire, 2:TagNotFoundException tnfe),
         
     list<DataSample> read_raw_datas(1:required string tagname, 
                                     2:required double start_time, 
                                     3:required double end_time)
-        throws (1:InvalidRequestException ire),
+        throws (1:InvalidRequestException ire, 2:TagNotFoundException tnfe),
         
     list<DataSample> read_sampled_datas(1:required string tagname, 
                                         2:required double start_time, 
                                         3:required double end_time, 
                                         4:required i64 number_of_samples, 
-                                        5:required SamplingMode mode=SamplingMode.INTERPOLATED)
-        throws (1:InvalidRequestException ire),
+                                        5:required SamplingType type=SamplingType.INTERPOLATED)
+        throws (1:InvalidRequestException ire, 2:TagNotFoundException tnfe),
     
     list<DataQuality> write_datas(1:required list<DataSample> data_samples)
-        throws (1:InvalidRequestException ire),
+        throws (1:InvalidRequestException ire, 2:TagNotFoundException tnfe),
 }
